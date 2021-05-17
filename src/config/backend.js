3f2fc36420482983f2a1d5ab2fcaee9d820103af
Namrata Gupta <NGu@c-nb-122.local>
2021-05-17 15:38:26 +0200

    chores: update oidc config

--- src/config/backend.js
+++ src/config/backend.js
@@ -1,4 +1,4 @@
 export const backendSettings = {
-  host: 'https://int-br.apps.openshift-03.knowis.cloud/CPORTBE/api/v1'
+  host: 'https://qa-br.apps.openshift-03.knowis.cloud/CPORTBE/api/v1'
   // host: 'http://localhost:3000'
 }
