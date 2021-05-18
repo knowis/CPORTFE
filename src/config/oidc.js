152dbb537f58c86d7c3238b5ce1e12dfde4c4b8d
Namrata Gupta <ngu@knowis.de>
2021-05-18 14:09:59 +0200

    chores: remove beforeall

--- src/config/oidc.js
+++ src/config/oidc.js
@@ -1,7 +1,6 @@
 export const oidcSettings = {
   authority: 'https://auth.apps.openshift-03.knowis.cloud/auth/realms/qa-designer',
   clientId: 'portal-ui',
-  // redirectUri: 'https://cportfe-qa-br.apps.openshift-03.knowis.cloud:443/oidc-callback',
   redirectUri: 'https://cportfe-qa-br.apps.openshift-03.knowis.cloud:443/oidc-callback',
   responseType: 'id_token token',
   scope: 'openid profile'
