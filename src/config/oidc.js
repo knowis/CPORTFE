5a20604cbd4289e6f71e9965c5df19365d233973
Namrata Gupta <ngu@knowis.de>
2021-05-20 11:11:23 +0200

    chores: update oidc config

--- src/config/oidc.js
+++ src/config/oidc.js
@@ -1,5 +1,5 @@
 export const oidcSettings = {
-  authority: 'https://auth.apps.openshift-03.knowis.cloud/auth/realms/qa-designer',
+  authority: 'https://auth.apps.openshift-03.knowis.cloud/auth/realms/qa-br',
   clientId: 'portal-ui',
   redirectUri: 'https://cportfe-qa-br.apps.openshift-03.knowis.cloud:443/oidc-callback',
   responseType: 'id_token token',
