832dc37c4509909414394a7f0a5219e53332d85b
Namrata Gupta <ngu@knowis.de>
2021-05-21 15:14:26 +0200

    chores: update config for qa

--- src/config/oidc.js
+++ src/config/oidc.js
@@ -1,8 +1,8 @@
 export const oidcSettings = {
-  authority: 'https://keycloak-edu-keycloak.apps.openshift-01.knowis.cloud/auth/realms/education',
+  authority: 'https://auth.apps.openshift-03.knowis.cloud/auth/realms/qa-br',
   // update clientId and redirectUri based on the client created in keycloak
   clientId: 'portal-ui',
-  redirectUri: 'https://cportfe-edu-keycloak.apps.openshift-03.knowis.cloud:443/oidc-callback',
+  redirectUri: 'https://cportfe-qa-br.apps.openshift-03.knowis.cloud:443/oidc-callback',
   responseType: 'id_token token',
   scope: 'openid profile'
 }
