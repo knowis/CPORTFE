4fb8f18ac21d78421cfe477e2f518293e00d5d47
Namrata Gupta <ngu@knowis.de>
2021-06-08 15:32:24 +0200

    chores: update .gitattributes file

--- src/config/oidc.js
+++ src/config/oidc.js
@@ -1,7 +1,7 @@
 export const oidcSettings = {
-  authority: 'https://auth.apps.openshift-03.knowis.cloud/auth/realms/release',
+  authority: 'https://auth.apps.openshift-03.knowis.cloud/auth/realms/qa-br',
   clientId: 'portal-ui',
-  redirectUri: 'https://release-cportfe.apps.openshift-03.knowis.cloud:443/oidc-callback',
+  redirectUri: 'https://cportfe-qa-br.apps.openshift-03.knowis.cloud:443/oidc-callback',
   responseType: 'id_token token',
   scope: 'openid profile'
 }
