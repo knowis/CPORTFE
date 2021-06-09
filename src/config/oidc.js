7e70d0124077b524dfc85af3b5dbbf2dd591c24c
Namrata Gupta <ngu@knowis.de>
2021-06-09 12:05:10 +0200

    chores: update oidc config

--- src/config/oidc.js
+++ src/config/oidc.js
@@ -1,8 +1,8 @@
 export const oidcSettings = {
-  authority: 'https://auth.apps.openshift-03.knowis.cloud/auth/realms/release',
+  authority: 'https://auth.apps.openshift-03.knowis.cloud/auth/realms/qa-br',
   // update clientId and redirectUri based on the client created in keycloak
   clientId: 'portal-ui',
-  redirectUri: 'https://release-cportfe.apps.openshift-03.knowis.cloud:443/oidc-callback',
+  redirectUri: 'https://cportfe-qa-br.apps.openshift-03.knowis.cloud:443/oidc-callback',
   responseType: 'id_token token',
   scope: 'openid profile'
 }
